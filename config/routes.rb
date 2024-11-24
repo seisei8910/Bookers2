49399a855092e7d200191f05511c25af8ab8e1ed
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-11-24 03:29:52 +0000

    [Start] bookのindexページの左側の作成(User info)(New book)

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
   devise_for :users
-  get '/users', to: 'users#index', as: :users
-
+  resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
