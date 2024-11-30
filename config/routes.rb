2ff56bcf9f3bafecf8d4c3d1bd73b2695b3a39e8
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2024-11-30 14:17:32 +0000

    [Finish] RSpecテスト実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'homes/about', to: 'homes#about', as: :about
+  get 'home/about', to: 'homes#about', as: :about
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
